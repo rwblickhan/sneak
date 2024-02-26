8ef76852daa3c8057fc13443dca9481152fa503f
Russell Blickhan <rwblickhan@gmail.com>
2024-02-25 21:21:24 -0800

    Remove unused background script

--- entrypoints/background.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "@/types/types";
-
-export default defineBackground(() => {
-  async function handleOpenLink(message: Message) {
-    await browser.tabs.create({ url: message.url });
-  }
-  browser.runtime.onMessage.addListener(handleOpenLink);
-});
--- types/types.ts
+++
@@ -1,4 +0,0 @@
-export type MessageId = "open_link";
-export interface Message {
-  url: string;
-}
